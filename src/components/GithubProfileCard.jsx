d454c2fddc6e782f3b853541748713e2dbf5604c
NukeNalin <nalinporwal10@gmail.com>
2021-06-06 00:31:08 +0530

    Adding Footer Details

--- src/components/GithubProfileCard.jsx
+++ src/components/GithubProfileCard.jsx
@@ -16,21 +16,21 @@ const GithubProfileCard = ({prof}) => {
                 <div className="p-2">
                   <Row className="">
                   <Col className="order-lg-2" lg="4">
-                      <img src={prof.avatar_url} style={{width: "200px"}} alt="" className="rounded-circle img-center img-fluid shadow shadow-lg--hover mb-4"/>
+                      <img src={"https://pbs.twimg.com/profile_images/1193947930777903105/BSpVAZdg_400x400.jpg"} style={{width: "200px"}} alt="" className="rounded-circle img-center img-fluid shadow shadow-lg--hover mb-4"/>
                     </Col>
                     <Col lg="8" className="order-lg-1">
                       <h2 className="text-white">
                         Reach Out to me!
                       </h2>
                       <p className="lead text-white mt-3">
-                        DISCUSS A PROJECT OR JUST WANT TO SAY HI? MY INBOX IS OPEN FOR ALL
+                         Drop E-Mail on nalinporwal10@gmail.com
                       </p>
                       <p className="text-white mt-3">
-                          {prof.bio}
+                          I'am iOS Developer | SwiftUI | Combine
                       </p>
                       <div className="my-3 icon-shape bg-gradient-white shadow rounded text-info">
                         <i className="ni ni-pin-3 text-info mr-2" />
-                        {prof.location}
+                        Indore India
                         </div>
                         <SocialLinks />
                     </Col>                    
